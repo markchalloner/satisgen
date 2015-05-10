7d748e3a489cac8a7e1a9f5d61c3cfc179993d6a
Mark Challoner <mark.a.r.challoner@gmail.com>
2015-05-10 15:37:33 +0100

    Updated autoloader

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,27 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+/*
+ * This file is part of Satis.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use SatisGen\Application\GenerateApplication;
-use SatisGen\Command\GenerateCommand;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Filesystem\Filesystem;
+function includeIfExists($file)
+{
+    if (file_exists($file)) {
+        return include $file;
+    }
+}
 
-$application = new GenerateApplication(new Filesystem());
-$application->run();
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    print('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+          'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+          'php composer.phar install'.PHP_EOL);
+    die(1);
+}
+
+return $loader;
