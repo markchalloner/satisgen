94d474abb72f932c55aa20b1a0d0c23899637296
Mark Challoner <mark.a.r.challoner@gmail.com>
2015-06-07 14:07:00 +0100

    Rolled back "fix" for code issue.

--- src/Config/AdvancedConfigReaderInterface.php
+++ src/Config/AdvancedConfigReaderInterface.php
@@ -4,8 +4,6 @@ namespace SatisGen\Config;
 
 interface AdvancedConfigReaderInterface {
 
-    public function getInputReader(InputReaderInterface $inputReader);
-
     public function getConfig($name, $description, $default = null, $validator = null, $secure = false);
 
 }
